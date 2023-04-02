ca7f20b85cece517fc0b14f0de462e07a13964a3
Ldelara <ligia.delara15@gmail.com>
2023-04-02 19:43:11 -0400

    fixed layout from js update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-let cityElement = document.querySelector("#cityName");
+function updateTime() {
+  //change city html
+  let cityElement = document.querySelector("#cityName");
+  if (cityElement) {
+    let dateElement = cityElement.querySelector(".date");
+    let timeElement = cityElement.querySelector(".time");
+    let createdTime = moment.tz.setDefault();
+
+    dateElement.innerHTML = createdTime.format("MMMM	Do YYYY");
+    timeElement.innerHTML = createdTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
 
 //change the city once it's been selected
 function updateCity(event) {
@@ -11,9 +24,9 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector(".cities");
   citiesElement.innerHTML = `
-  <div class="city">
+  <div class=">
     <div>
-      <h2 id="cityName">${cityName}</h2>
+      <h2 id="city-name">${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
     <div class="time">${cityTime.format(
@@ -22,6 +35,8 @@ function updateCity(event) {
   </div>
   `;
 }
+updateTime();
+setInterval(updateTime, 1);
 //event listener on the selector
 let citySelect = document.querySelector("#choose-city");
 citySelect.addEventListener("change", updateCity);
